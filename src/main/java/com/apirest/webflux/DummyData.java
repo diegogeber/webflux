a62e19f05888e588b38a7e08d7bca7b40ff383b0
diegogeber <diegogeber@gmail.com>
2020-02-16 09:52:34 -0300

    first commit

---
+++ src/main/java/com/apirest/webflux/DummyData.java
@@ -0,0 +1,34 @@
+package com.apirest.webflux;
+
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.repository.PlaylistRepository;
+
+import reactor.core.publisher.Flux;
+
+@Component
+public class DummyData implements CommandLineRunner{
+	
+	
+	private final PlaylistRepository playlistRepository;
+	
+	public DummyData(PlaylistRepository playlistRepository) {
+		this.playlistRepository = playlistRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+				playlistRepository.deleteAll()
+					.thenMany(Flux.just("Rock","Pop","Samba","Funk","Nacional","Internacional")
+								  		.map(name -> new Playlist(UUID.randomUUID().toString(), name))
+								  		.flatMap(playlistRepository::save))
+					.subscribe(System.out::println);
+	}
+	
+	
+}
